d8d13854766f99c228c6b81f6bc05de83784bb23
Ben Wiedeback <66647604+Inlic@users.noreply.github.com>
2020-12-17 16:48:07 -0700

    template and object creation

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 const IMG_PATH = 'https://image.tmbdb.org/t/p/w1280'
 const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=36fc55a0ce46207ac9d6afb7b2112f39&query="'
 
+const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 
@@ -11,6 +12,32 @@ async function getMovies(url) {
   const res = await fetch(url)
   const data = await res.json()
 
+
+}
+
+function showMovies(movies) {
+  main.innerHTML = ''
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie
+  })
+  const movieElement = document.createElement('div')
+  movieElement.classList.add('movie')
+  movie.innerHTML = `
+  <div class="movie">
+      <img
+        src="https://images.unsplash.com/photo-1518173835740-f5d14111d76a?ixlib-rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1154&q=80"
+        alt="">
+      <div class="movie-info">
+        <h3>Movie Title</h3>
+        <span class="green">9.8</span>
+      </div>
+      <div class="overview">
+        <h3>Overview</h3>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, aperiam perspiciatis! Nobis, nemo nulla tenetur
+        aperiam enim maiores voluptas accusantium.
+      </div>
+    </div>  
+  `
 
 }
 
